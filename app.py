02bcb634ffe118c711295d74a0bf9025990ef8c1
Benjamin Lau <ben_lau93@hotmail.com>
2022-08-28 11:48:43 +0800

    change app title

--- app.py
+++ app.py
@@ -5,5 +5,5 @@ import dash_bootstrap_components as dbc
 external_stylesheets = [dbc.themes.LUX]
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets,suppress_callback_exceptions=True)
-
+app.title = "Trading Dashboard"
 server = app.server
